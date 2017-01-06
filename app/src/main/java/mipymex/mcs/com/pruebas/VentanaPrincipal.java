e10e13aa20ee4fe9ea23081da9652d2d62df8c6b
CesarHL <cesar.herlop@yahoo.com.mx>
2017-01-06 16:09:55 -0600

    refactor a tablayout

--- app/src/main/java/mipymex/mcs/com/pruebas/VentanaPrincipal.java
+++ app/src/main/java/mipymex/mcs/com/pruebas/VentanaPrincipal.java
@@ -1,26 +1,17 @@
 package mipymex.mcs.com.pruebas;
 
-import android.app.TabActivity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.TabHost;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
-
-
+import android.support.v7.app.AppCompatActivity;
 
 
 /**
  * Created by Erick on 02/01/2017.
  */
 
-@SuppressWarnings("deprecation")
-public class VentanaPrincipal extends TabActivity {
+
+public class VentanaPrincipal extends AppCompatActivity {
 
     private ViewPager viewPager;
 
@@ -29,68 +20,56 @@ public class VentanaPrincipal extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_principal);
 
-        final TabHost host = (TabHost) findViewById(android.R.id.tabhost);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
-        viewPager  = (ViewPager)    findViewById(R.id.view_pager);
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                host.setCurrentTab(position);
-            }
-            @Override
-            public void onPageScrolled(
-                    int position, float positionOffset, int positionOffsetPixels) { }
-            @Override
-            public void onPageScrollStateChanged(int state) {}
-        });
-        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                int selectItem = host.getCurrentTab();
-                viewPager.setCurrentItem(selectItem);
-            }
-        });
+        tabLayout.addTab(tabLayout.newTab().setText("Solicitante"));
+        tabLayout.addTab(tabLayout.newTab().setText("Laboral"));
+        tabLayout.addTab(tabLayout.newTab().setText("Cónyuge"));
+        tabLayout.addTab(tabLayout.newTab().setText("Referencias"));
+        tabLayout.addTab(tabLayout.newTab().setText("Referencias F"));
+        tabLayout.addTab(tabLayout.newTab().setText("Firma"));
+        tabLayout.addTab(tabLayout.newTab().setText("Imágenes"));
 
 
 
-        host.addTab(host.newTabSpec("tab1").setIndicator("Solicitante").setContent(
-                new Intent(this, InformacionSolicitante.class)));
-
-        host.addTab(host.newTabSpec("tab2").setIndicator("Laboral").setContent(
-                new Intent(this, InformacionLaboral.class)));
-
-        host.addTab(host.newTabSpec("tab3").setIndicator("Conyuge").setContent(
-                new Intent(this, DatosConyugeHijos.class)));
-
-        host.addTab(host.newTabSpec("tab4").setIndicator("Referencias").setContent(
-                new Intent(this, ReferenciasPersonales.class)));
-
-        host.addTab(host.newTabSpec("tab5").setIndicator("Referencias F").setContent(
-                new Intent(this, ReferenciasPersonalesDistintoDomicilio.class)));
-
-        host.addTab(host.newTabSpec("tab6").setIndicator("Firma").setContent(
-                new Intent(this, CaptureSignature.class)));
-
-        host.addTab(host.newTabSpec("tab7").setIndicator("Imagenes").setContent(
-                new Intent(this, CatalogoImagenes.class)));
+//        host.addTab(host.newTabSpec("tab1").setIndicator("Solicitante").setContent(
+//                new Intent(this, InformacionSolicitante.class)));
+//
+//        host.addTab(host.newTabSpec("tab2").setIndicator("Laboral").setContent(
+//                new Intent(this, InformacionLaboral.class)));
+//
+//        host.addTab(host.newTabSpec("tab3").setIndicator("Conyuge").setContent(
+//                new Intent(this, DatosConyugeHijos.class)));
+//
+//        host.addTab(host.newTabSpec("tab4").setIndicator("Referencias").setContent(
+//                new Intent(this, ReferenciasPersonales.class)));
+//
+//        host.addTab(host.newTabSpec("tab5").setIndicator("Referencias F").setContent(
+//                new Intent(this, ReferenciasPersonalesDistintoDomicilio.class)));
+//
+//        host.addTab(host.newTabSpec("tab6").setIndicator("Firma").setContent(
+//                new Intent(this, CaptureSignature.class)));
+//
+//        host.addTab(host.newTabSpec("tab7").setIndicator("Imagenes").setContent(
+//                new Intent(this, CatalogoImagenes.class)));
 
     }
 
-    public class FakeContent implements TabHost.TabContentFactory{
-
-        Context context;
-
-    public FakeContent(Context context){
-        this.context = context;
-    }
-    @Override
-    public View createTabContent(String tag) {
-        View fakeView = new View(context);
-        fakeView.setMinimumWidth(0);
-        fakeView.setMinimumHeight(0);
-        return fakeView;
-    }
-}
+//    public class FakeContent implements TabHost.TabContentFactory{
+//
+//        Context context;
+//
+//        public FakeContent(Context context){
+//            this.context = context;
+//        }
+//        @Override
+//        public View createTabContent(String tag) {
+//            View fakeView = new View(context);
+//            fakeView.setMinimumWidth(0);
+//            fakeView.setMinimumHeight(0);
+//            return fakeView;
+//        }
+//    }
 
 }
 
